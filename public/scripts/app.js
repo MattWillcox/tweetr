ee53bda321382fde78f13085f598e5c5bb1013aa
Matt Willcox <mattjwillcox@gmail.com>
2017-08-08 23:25:40 +0000

    Mentor code review refactoring

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -56,18 +56,18 @@ $(function(){
     }
   }
 
-  function createTweetHeader(tweet) {
+  function createTweetHeader(user) {
       let $tweetHeader = $('<header>');
-      $tweetHeader.append(`<img src=${tweet.user.avatars.small}>`)
-      .append(`<h2 class='user-name'>${tweet.user.name}</h2>`)
-      .append(`<span class='handle'>${tweet.user.handle}</span>`);
+      $tweetHeader.append(`<img src=${user.avatars.small}>`)
+      .append(`<h2 class='user-name'>${user.name}</h2>`)
+      .append(`<span class='handle'>${user.handle}</span>`);
       return $tweetHeader;
   }
 
-  function createTweetFooter(tweet) {
+  function createTweetFooter(created_at) {
     let $tweetFooter = $('<footer>');
     $tweetFooter.append("<div class='tweet-bottom-wrapper'>")
-    let timeStamp = $.format.date(tweet.created_at, "dd/MM/yyyy HH:mm:ss");
+    let timeStamp = $.format.prettyDate(created_at);
     $tweetFooter.find('.tweet-bottom-wrapper').append(`<span class='tweet-bottom'>${timeStamp}`);
     $tweetFooter.find('.tweet-bottom').append('<a href="/">Report</a><a href="/">Retweet</a><a href="/">Like</a>');
     return $tweetFooter;
@@ -75,10 +75,10 @@ $(function(){
 
   function createTweetElement(tweet) {
     let $newTweet = $('<article class="tweet">');
-    $newTweet.append(createTweetHeader(tweet));
+    $newTweet.append(createTweetHeader(tweet.user));
     let tweetContent = $('<div class="tweet-content">').text(tweet.content.text);
     $newTweet.append(tweetContent);
-    $newTweet.append(createTweetFooter(tweet))
+    $newTweet.append(createTweetFooter(tweet.created_at));
     return $newTweet;
   }
 
