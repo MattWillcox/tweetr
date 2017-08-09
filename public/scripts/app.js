739f8332f5492daa6e0ca49416f5a9de0fa0e2d4
Matt Willcox <mattjwillcox@gmail.com>
2017-08-09 01:28:32 +0000

    adds tweet fetching functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,5 @@
 $(function(){
 
-  var data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants. This is some extra stuff to make this super long."
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "<script>alert('uh oh!');</script>"
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
   function renderTweets(tweets) {
     // loops through tweets
       // calls createTweetElement for each tweet
@@ -82,7 +35,11 @@ $(function(){
     return $newTweet;
   }
 
-renderTweets(data);
+  function loadTweets(){
+    $.get("/tweets", (data) => renderTweets(data));
+  }
+
+loadTweets();
 
 $('.add-tweet').on('submit', function(event) {
   event.preventDefault();
